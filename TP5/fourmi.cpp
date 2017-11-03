055fadab3bc5d690f3726d9b1e79767093641b90
Xinwei Chai <xinwei.chai@hotmail.com>
2017-11-03 17:36:41 +0100

    TP5

--- TP5/fourmi.cpp
+++ TP5/fourmi.cpp
@@ -2,16 +2,44 @@
 #include "fourmi.h"
 #include "constantes.h"
 
-void DeplaceFourmi(t_fourmi f){
-	int comportement=nalea(8);
-	switch (comportement){
-		case 0 : --f.x;--f.y;
-		case 1 : --f.y;
-		case 2 : ++f.x;--f.y;
-		case 3 : ++f.x;
-		case 4 : ++f.x;++f.y;
-		case 5 : ++f.y;
-		case 6 : --f.x;++f.y;
-		case 7 : --f.x;
-	}
+int modulo8(int x){
+	return ((x+8)%8);
 }
+
+bool PositionPossible(int x, int y, t_monde m){
+	return !((x<0)||(y<0)||(x==m.L)||(y==m.H)||(m.mat[x][y]==-1));
+}
+
+t_fourmi DeplaceFourmi(t_fourmi fourmi, t_monde m){
+	int comportement=nalea(8);
+//	switch (comportement){
+//		case 0 : --f.x;--f.y;
+//		case 1 : --f.y;
+//		case 2 : ++f.x;--f.y;
+//		case 3 : ++f.x;
+//		case 4 : ++f.x;++f.y;
+//		case 5 : ++f.y;
+//		case 6 : --f.x;++f.y;
+//		case 7 : --f.x;
+//	}
+	int tdx[]={1,1,0,-1,-1,-1,0,1}, 
+	    tdy[]={0,1,1,1,0,-1,-1,-1},
+	    p_toutdroit[]={12,2,1,1,0,1,1,2},
+	    ponderation[8];
+	int dir,i;
+	for (i=0;i<8;++i){
+		dir = modulo8(i - fourmi.direction);
+		ponderation[i] = p_toutdroit[dir];
+	}
+	for (i=0;i<8;++i){
+		if (!PositionPossible(fourmi.x+tdx[i],fourmi.y+tdy[i],m))
+			ponderation[i]=0;
+	}
+	fourmi.direction=nalea_pondere(ponderation);
+	fourmi.x+=tdx[fourmi.direction];
+	fourmi.y+=tdy[fourmi.direction];
+	return fourmi;
+}
+
+
+
--- TP5/fourmi.h
+++ TP5/fourmi.h
@@ -1,5 +1,6 @@
 struct t_fourmi{
 	int x,y;
+	int direction;
 	int status;// 0 for go and 1 for back
 };
 
