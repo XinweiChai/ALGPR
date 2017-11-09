174eb89a6e4192908a6ea6e6737c77cb23c9a3bd
Xinwei Chai <xinwei.chai@hotmail.com>
2017-11-09 17:05:10 +0100

    TP5 finished

--- TP5/fourmi.h
+++ TP5/fourmi.h
@@ -1,7 +1,27 @@
-struct t_fourmi{
-	int x,y;
-	int direction;
-	int mode;// 1 for go and 0 for back
+/*************************************************************
+
+               Fichier entete de fourmi.cpp
+
+    ------------------------------------------------------
+ Auteurs : équipe pédagogique ALGPR
+ Date : 01/10/2009
+ Modifié : 10/04/2013, 04/11/2013 (Vincent Tourre)
+ Fichier: fourmi.h
+ But: définition du type fourmi et déclaration des fonctions de déplacement de la fourmi
+ 
+*************************************************************/
+
+#ifndef FOURMI_H
+#define FOURMI_H
+
+struct t_fourmi
+{
+	int x,y;		// position de la fourmi
+	int direction;	// direction de la fourmi (son orientation)
+	int recherche;	// indique si la fourmi est en recherche de nourriture (=1) ou si elle porte de la nourriture (=0)
 };
 
-void DeplaceFourmi(t_fourmi);
+void DeplaceFourmi( t_fourmi& fourmi, t_monde& environnement, t_matrice pheromones);
+
+
+#endif
--- TP5/monde.cpp
+++ TP5/monde.cpp
@@ -1,19 +1,72 @@
-#include <string>
+/*************************************************************
+
+         Lecture de l'environnement dans un fichier
+         et Evaporation de la phéromone
+
+    ------------------------------------------------------
+
+ Auteur: HILAIRE Thibault, Mikaèl BRIDAY
+ Date: 01/10/2003
+ Révisé et porté en C par Didier LIME le 7/05/2009
+ modifié : 10/04/2013 Vincent Tourre
+ Fichier: monde.cpp
+ But: Fonctions permettant de lire l'environnement depuis un
+      fichier, et permettant de simuler l'évaporation de
+      phéromones
+
+*************************************************************/
+
+
 #include <fstream>
 #include "constantes.h"
 
+#include "monde.h"
+
 using namespace std;
 
-t_monde LireEnvironnement(string filename){
-	ifstream f(filename.c_str());
-	t_monde m;	
-	f >> m.L >> m.H;
-	f >> m.Fx >> m.Fy;
-	for (int i=0;i<m.H;++i){
-		for (int j=0;j<m.L;++j){
-			f >> m.mat[i][j];
+//action: LireEnvironnement
+//arguments: environnement : t_monde représentant l'environnement (qui va Ítre remplit par l'action)
+//but: permet de lire l'environnement depuis un fichier
+//principe: lecture de la taille du tableau, puis du tableau (depuis le fichier)
+t_monde LireEnvironnement()
+{
+	int i,j;
+	ifstream file;
+	t_monde environnement;
+
+	// ouverture du fichier
+	file.open("monde1.dat");
+	//file.open("monde2.dat");
+	//file.open("monde3.dat");
+
+	// lire hauteur et largeur
+	file >> environnement.L;	//largeur
+	file >> environnement.H;	//hauteur
+
+	// lire la position de la fourmilière
+	file >> environnement.Fx;
+	file >> environnement.Fy;
+	
+	// puis lire le tableau
+	for(j=0;j<environnement.H;j++) //pour tte les lignes => y
+		for(i=0;i<environnement.L;i++)	//pour tte les colonnes => x
+		{
+			file >> environnement.mat[i][j];	
 		}
-	}
-	f.close();
-	return m;
+
+    return environnement;
 }
+
+
+//action: DecroissancePheromones
+//arguments: pheromones : matrice où la valeur des pheromones dans le monde est stocké (modifié par l'action)
+//but: permet de simuler l'évaporation des pheromones
+//principe: on décrémente (de 1) le niveau de phéromones, lorsque celui-ci est strictement positif (phéromone présente)
+void EvaporationPheromones( t_matrice pheromones, int L, int H)
+{
+	int i,j;
+	for(j=0;j<H;j++)
+		for(i=0;i<L;i++)
+			if (pheromones[i][j]>0)	pheromones[i][j]--;
+}
+
